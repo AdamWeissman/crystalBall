196e8439fb00f40ac2010ed58d2c10512d39f020
AdamWeissman <adam@comedytragedyepic.com>
2020-12-28 16:20:10 -0500

    maybe I don't need backend for this test project

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const express = require("express");
-const app = express();
-const path = require("path")
+// const express = require("express");
+// const app = express();
+// const path = require("path")
 
-// app.use((request, response) => {
-//   console.log("we got a new request")
-//   // response.send(`<iframe width="560" height="315" src="https://www.youtube.com/embed/raleKODYeg0?controls=0&amp;start=${Math.floor(Math.random() * 120)}" frameborder="0" allow="accelerometer; autoplay=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`)
-//   response.send("testing")
+// // app.use((request, response) => {
+// //   console.log("we got a new request")
+// //   // response.send(`<iframe width="560" height="315" src="https://www.youtube.com/embed/raleKODYeg0?controls=0&amp;start=${Math.floor(Math.random() * 120)}" frameborder="0" allow="accelerometer; autoplay=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`)
+// //   response.send("testing")
+// // })
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/index.html'))
 // })
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'))
-})
 
 
 
-
-app.listen(3000, () => {
-  console.log("listening on port 3000")
+// app.listen(3000, () => {
+//   console.log("listening on port 3000")
 
 
 
 
 
-})
+// })
