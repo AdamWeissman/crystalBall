5df5d7184b7abcb3829ecd5e51b37b817a4f7248
AdamWeissman <adam@comedytragedyepic.com>
2020-12-28 16:31:09 -0500

    updates with threejs

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+import * as THREE from '../../libs/three/three.module.js';
+import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
+
+class App{
+	constructor(){
+		const container = document.createElement( 'div' );
+		document.body.appendChild( container );
+    
+        window.addEventListener('resize', this.resize.bind(this) );
+	}	
+    
+    resize(){
+        
+    }
+    
+	render( ) {   
+        
+    }
+}
+
+export { App };
+
+
 // const express = require("express");
 // const app = express();
 // const path = require("path")
