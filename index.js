a91ac3eac4ef8fd6bf985b40ab04a27d93326147
AdamWeissman <adam@comedytragedyepic.com>
2020-12-29 16:25:25 -0500

    huge successgit add .!  dynamically altering variables

--- index.js
+++ index.js
@@ -18,14 +18,16 @@ app.get('/', (req, res) => {
 })
 
 app.get('/theFuture', (req, res) => {
-  const start = 10 + Math.floor(Math.random() * 100)
-  const finish = start + 21
+  const start = 10 + Math.floor(Math.random() * 100);
+  const finish = start + 11 + Math.floor(Math.random() * 13);
+  const animationLength = (finish - start).toString();
+  
   const hash = { equilibrium: 'raleKODYeg0', independenceDay: 'B1E7h3SeMDk', iAmLegend: 'dtKMEAXyPkg', soylentGreen: 'N_jGOKYHxaQ', metropolis: 'GrFBId1b8U0', worldWarZ: 'Md6Dvxdr0AQ', hungerGames1: 'mfmrPu43DF8', roboCop: 'NFWgbGVgozA', bladeRunner: 'eogpIG53Cis', bladeRunner2049: 'gCcx85zbxz4'}
   const changeMeUp = function(arr) {
     return arr[Math.floor(Math.random() * arr.length)]
   }(Object.values(hash))
   
-  res.render('theFuture.ejs', { begin: start, end: finish, theLink: changeMeUp})
+  res.render('theFuture.ejs', { begin: start, end: finish, theLink: changeMeUp, animationLength: animationLength})
   
 })
 
