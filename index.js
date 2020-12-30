04aaeb921189c05ad6207f4ff3bc648432903dc6
AdamWeissman <adam@comedytragedyepic.com>
2020-12-30 01:14:34 -0500

    environment changes

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ const app = express();
 const path = require("path")
 const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 app.use(express.static(path.join(__dirname, 'public')))
 
