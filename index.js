5def67ed9d4ad9842b3ab4c1965306c5a4350a2a
AdamWeissman <adam@comedytragedyepic.com>
2020-12-29 22:27:25 -0500

    setting up public directory for css folders

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const path = require("path")
 //   response.send("testing")
 // })
 
+app.use(express.static(path.join(__dirname, 'public')))
+
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
 
@@ -19,7 +22,7 @@ app.get('/', (req, res) => {
 
 app.get('/theFuture', (req, res) => {
   const start = 10 + Math.floor(Math.random() * 100);
-  const finish = start + 5 + Math.floor(Math.random() * 10);
+  const finish = start + 11 + Math.floor(Math.random() * 10);
   const animationLength = (finish - start).toString();
   
   const hash = { equilibrium: 'raleKODYeg0', independenceDay: 'B1E7h3SeMDk', iAmLegend: 'dtKMEAXyPkg', metropolis: 'GrFBId1b8U0', worldWarZ: 'Md6Dvxdr0AQ'}
@@ -29,49 +32,46 @@ app.get('/theFuture', (req, res) => {
     return arr[Math.floor(Math.random() * arr.length)]
   }(Object.values(hash))
 
-  let changeMeUp2 = null
+  // let changeMeUp2 = null
 
-  const loadUpWithDelay = (movieId, delay) => {
-    return new Promise ((resolve) => {
-      setTimeout(() => {
-        changeMeUp2 = movieId
-        resolve();
-      }, delay)
-    })
-  }
+  // const loadUpWithDelay = (movieId, delay) => {
+  //   return new Promise ((resolve) => {
+  //     setTimeout(() => {
+  //       changeMeUp2 = movieId
+  //       resolve();
+  //     }, delay)
+  //   })
+  // }
 
-  const tempChoices = [...(Object.values(hash))]
-  function getThree(arr) {
-    const theseChoices = []
-    theseChoices.push(arr.pop([Math.floor(Math.random() * arr.length)]));
-    theseChoices.push(arr.pop([Math.floor(Math.random() * arr.length)]));
-    theseChoices.push(arr.pop([Math.floor(Math.random() * arr.length)]));
-    console.log("the array receiving the ids inside of getThree", theseChoices)
-    console.log("this is the array I'm expecting is modified in place", arr)
-    return theseChoices
-  }
+  // const tempChoices = [...(Object.values(hash))]
+  // function getThree(arr) {
+  //   const theseChoices = []
+  //   theseChoices.push(arr.pop([Math.floor(Math.random() * arr.length)]));
+  //   theseChoices.push(arr.pop([Math.floor(Math.random() * arr.length)]));
+  //   theseChoices.push(arr.pop([Math.floor(Math.random() * arr.length)]));
+  //   console.log("the array receiving the ids inside of getThree", theseChoices)
+  //   console.log("this is the array I'm expecting is modified in place", arr)
+  //   return theseChoices
+  // }
 
-  console.log("these were all the original choices", tempChoices)
-  const theFutureArray = getThree(tempChoices)
+  // console.log("these were all the original choices", tempChoices)
+  // const theFutureArray = getThree(tempChoices)
 
-  async function rotateFutures(arr) {
-    // await loadUpWithDelay(arr[0], animationLength * 1000)
-    changeMeUp2 = theFutureArray[0]
-    console.log(changeMeUp2)
-    res.render('theFuture.ejs', { begin: start, end: finish, theLink: changeMeUp2, animationLength: animationLength})
-    await loadUpWithDelay(arr[1], animationLength * 2050)
-    console.log(changeMeUp2)
-    res.render('theFuture.ejs', { begin: start, end: finish, theLink: changeMeUp2, animationLength: animationLength})
-    await loadUpWithDelay(arr[2], animationLength * 1000)
-    console.log(changeMeUp2)
-    res.render('theFuture.ejs', { begin: start, end: finish, theLink: changeMeUp2, animationLength: animationLength})
+  // async function rotateFutures(arr) {
+  //   // await loadUpWithDelay(arr[0], animationLength * 1000)
+  //   changeMeUp2 = theFutureArray[0]
+  //   console.log(changeMeUp2)
+  //   res.render('theFuture.ejs', { begin: start, end: finish, theLink: changeMeUp2, animationLength: animationLength})
+  //   await loadUpWithDelay(arr[1], animationLength * 1000)
+  //   console.log(changeMeUp2)
+  //   await loadUpWithDelay(arr[2], animationLength * 1000)
+  //   console.log(changeMeUp2)
+  // }
 
-  }
+  // rotateFutures(theFutureArray)
 
-  rotateFutures(theFutureArray)
-  console.log(changeMeUp2)
   
-  // res.render('theFuture.ejs', { begin: start, end: finish, theLink: changeMeUp2, animationLength: animationLength})
+  res.render('theFuture.ejs', { begin: start, end: finish, theLink: changeMeUp, animationLength: animationLength})
   
 })
 
