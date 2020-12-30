a4d091608bf530d64b8724695dd28e93abde533d
AdamWeissman <adam@comedytragedyepic.com>
2020-12-30 01:54:47 -0500

    new clips

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/theFuture', (req, res) => {
   const animationLength = (finish - start).toString();
   
   const oldHash = { equilibrium: 'raleKODYeg0', independenceDay: 'B1E7h3SeMDk', iAmLegend: 'dtKMEAXyPkg', worldWarZ: 'Md6Dvxdr0AQ'}
-  const hash = { dystopianMontage1245: 'A1ZHl8qqg4A', top10worldEnd1300: '38D90SWwLcU'}
+  const hash = { dystopianMontage1245: 'A1ZHl8qqg4A', top10worldEnd1300: '38D90SWwLcU', futuristicCities: 'OeEqAx0FWA'}
   
   //this is for a single lnk
   const changeMeUp = function(arr) {
